a2814d019ac9939dd584b06462c136b073c26c20
Yanis <yayahamour@gmail.com>
2021-12-08 16:53:15 +0100

    Fonction liste de spell

--- character_core_mechanics.py
+++ character_core_mechanics.py
@@ -8,6 +8,22 @@ class CharacterCoreMechanics():
     _strenght : int
     _book : dict = field(default_factory=dict)
 
+
+#TODO a tester
+    def list_spell(self):
+        tab = []
+        if (self._book["Heal"][0] > 0):
+            tab.append("Heal")
+        if (self._book["Ice"][0] > 0):
+            tab.append("Ice")
+        if (self._book["Fire"][0] > 0):
+            tab.append("Fire")
+        if (self._book["Lightning"][0] > 0):
+            tab.append("Lightning")
+        return(tab)
+
+
+
     def base_attack(self, target):
         if(self._strenght > 0):
             life = target._life[0] - self._strenght
