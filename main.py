b7eea33f13ca03857fb3757a8b9f0c97228ad3d6
BuildTools <unconfigured@null.spigotmc.org>
2021-12-09 21:00:37 +0100

    first interface

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from dataclasses import dataclass
+import pygame
 import os
 
-@dataclass
-class eff():
-    erg : int
-
 def main():
-    string = "zeeazrzrzer"
-    for i in range(0,len(string)):
-        os.system('cls')
-        print(string[:i])
-
-print(eff(""))
+    pygame.init()
+    screen = pygame.display.set_mode((1440,900))
+    pygame.display.set_caption("Brightest Dungeon 2 Anniversary Collector Deluxe Edition")
+    launched = True
+    fond = pygame.image.load("image/menu.jpg")
+    font=pygame.font.Font(None, 24)
+    text = font.render("Brightest Dungeon 2",1,(255,255,255))
+    screen.blit(text, (300, 300))
+    pygame.display.flip()
+    screen.blit(fond, (0,0))
+    pygame.display.update()
+    
+    while launched:        
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                launched = False
+main()
